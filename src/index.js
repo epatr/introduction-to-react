f5298bf274a266b5b53b0b358b817dd7d0cf49c0
Eric Patrick <erstpa@gmail.com>
2019-03-14 21:07:34 -0400

    Now it's JSX

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-const HelloWorld = () => {
-    return React.createElement('header', null, 
-        React.createElement('h1', null, 'Hello World!')
-    );
-};
+// const HelloWorld = () => {
+//     return React.createElement('header', null, 
+//         React.createElement('h1', null, 'Hello World!')
+//     );
+// };
+
+const HelloWorld = () => 
+    <header>
+        <h1>Hello World</h1>
+    </header>;
 
 ReactDOM.render(
-    React.createElement(HelloWorld),
+    <HelloWorld />,
     document.querySelector('#root'),
 );
