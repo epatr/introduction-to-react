a3c908db99d2cecbf2151db000f4e55129cb723d
Eric Patrick <erstpa@gmail.com>
2019-03-18 19:58:42 -0400

    HelloWorld is now a component

--- src/index.js
+++ src/index.js
@@ -1,21 +1,7 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-// const HelloWorld = () => {
-//     return React.createElement('header', null, 
-//         React.createElement('h1', null, 'Hello World!')
-//     );
-// };
-
-const HelloWorld = () => 
-    <React.Fragment>
-        <header>
-            <h1>Hello World</h1>
-        </header>
-        <footer>
-            <small>Copyright 2019 Some Nonsense</small>
-        </footer>
-    </React.Fragment>;
+import HelloWorld from './components/HelloWorld';
 
 ReactDOM.render(
     <HelloWorld />,
