b9888f86a237307f60cae4c4215fed698c85e974
Eric Patrick <erstpa@gmail.com>
2019-03-27 20:51:09 -0400

    Added multi-line tertiary operator

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,9 +1,13 @@
 import * as React from 'react';
 
 export const ItemList = () => {
-    const items = ['red', 'green', 'blue'];
+    const items = []; // ['red', 'green', 'blue'];
 
     return <ul>
-        { items.map(item => <li key={item}>{item}</li>) }
+        { 
+            items.length === 0
+            ? <li>No items</li>
+            : items.map(item => <li key={item}>{item}</li>) 
+            }
     </ul>;
 };
